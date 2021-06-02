25467418aa4fb8b98bcf77370aa182f547d3a151
Kurt Lust <kurt.lust@kuleuven.be>
2021-06-02 20:19:40 +0200

    eat_mem.c reworked since it crashes on CentOS 8 due to too large stack variables, eat_disk.c and eat_cpu.c reformatted.

--- Slurm/11_Fine-tuning-Job-Specifications/eat_disk.c
+++ Slurm/11_Fine-tuning-Job-Specifications/eat_disk.c
@@ -11,22 +11,22 @@
 
 int main(int argc, char *argv[])
 {
-  FILE * fp = NULL;
+	FILE *fp = NULL;
 	unsigned long i;
-  char file_name[256] = "/tmp";
+	char file_name[256] = "/tmp";
 
-	char *value = getenv ("VSC_SCRATCH");
+	char *value = getenv("VSC_SCRATCH");
 	if (value)
 		strcpy(file_name, value);
-  strcat(file_name, "/test.txt");
-	printf ("Writing in file <%s>\n", file_name);
+	strcat(file_name, "/test.txt");
+	printf("Writing in file <%s>\n", file_name);
 
-	fp = fopen (file_name, "w+");
+	fp = fopen(file_name, "w+");
 	if (fp == NULL)
-    printf ("ERROR: Could not open file <%s>\n", file_name);
+		printf("ERROR: Could not open file <%s>\n", file_name);
 	for (i = 0; i < lines; i++)
-		fprintf(fp, "Line <%ld>: Random number: %d\n", i, rand()%1000);
+		fprintf(fp, "Line <%ld>: Random number: %d\n", i, rand() % 1000);
 	fclose(fp);
 
-  return 0;
+	return 0;
 }
--- Slurm/11_Fine-tuning-Job-Specifications/eat_mem.c
+++ Slurm/11_Fine-tuning-Job-Specifications/eat_mem.c
@@ -6,66 +6,95 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
-#define Kb 1024
-#define Mb 1024*Kb
-#define default_Gb 3
-#define max_Gb 12
-
+const unsigned int KiB = 1024;
+const unsigned int MiB = 1024 * KiB;
+const unsigned int default_GiB = 3;
+const unsigned int max_GiB = 12;
+const unsigned int long_sleep = 10;
+const unsigned int micro_sleep = 20000;
 
 int main(int argc, char *argv[])
 {
-  register int i;
-	int tot_Gb = default_Gb;
-  char* mem[Mb];
 
-	if (argc > 2) {
+	unsigned int tot_GiB = default_GiB;
+	char **mem;
+
+	if (argc > 2)
+	{
 		printf("Usage: eat_mem [number]\n");
-		printf("  Allocate and use [number] Gigabit of memory.\n");
-		printf("  Default is %s Gigabit.\n", default_Gb);
-	  exit(1);
+		printf("	Allocate and use [number] Gigabyte of memory.\n");
+		printf("	Default is %u Gigabyte.\n", default_GiB);
+		exit(1);
 	}
 
 	// Calculate the amount of memory to allocate
-	// Default = 5Gb
-	if (argc == 2) {
-		tot_Gb = atoi(argv[1]);
-		if (tot_Gb <= 0)
-		  tot_Gb = default_Gb;
-	  else if (tot_Gb > max_Gb) {
-			printf("Exceeded maximum, reset to %d Gigabit.\n", max_Gb);
-		  tot_Gb = max_Gb;
-	  }
+	// Default set by default_GiB
+	if (argc == 2)
+	{
+		int arg_GiB;
+		arg_GiB = atoi(argv[1]);
+		if (arg_GiB <= 0)
+			tot_GiB = default_GiB;
+		else if (arg_GiB > max_GiB)
+		{
+			printf("Exceeded maximum, reset to %d Gigabyte.\n", max_GiB);
+			tot_GiB = max_GiB;
+		}
+		else
+			tot_GiB = (unsigned int)arg_GiB;
 	}
-	printf("Consuming %d Gigabit of memory.\n", tot_Gb);
+	printf("Consuming %d GiB of memory.\n", tot_GiB);
 
-	char buffer[Mb];
+	// Allocate memory for mem
+	mem = (char **)calloc((size_t)MiB, sizeof(char *));
+	if (mem == NULL)
+	{
+		printf("ERROR: Failed to allocate memory for the internal variable mem\n");
+		return 1;
+	}
+
+	char *buffer;
+	buffer = (char *)malloc((size_t)MiB * sizeof(char));
+	if (buffer == NULL)
+	{
+		printf("ERROR: Failed to allocate memory for the internal variable buffer\n");
+		return 1;
+	}
 	// Fill up a buffer of 1 Mb with random digits
-	for (i = 0; i < Mb; i++)
-	  buffer[i] = rand()%256;
+	for (unsigned int i = 0; i < MiB; i++)
+		buffer[i] = rand() % 256;
 
-	// Consume memory, 1 Mb at a time
-  for (i = 0; i < tot_Gb*1024; i++) {
-		mem[i] = (char *)malloc(Mb*sizeof(char));
-		if (mem[i] == NULL) {
-			printf("ERROR: Out of memory\n");
+	// Consume memory, 1 MiB at a time
+	for (unsigned int i = 0; i < tot_GiB * 1024; i++)
+	{
+		mem[i] = (char *)malloc(MiB * sizeof(char));
+		if (mem[i] == NULL)
+		{
+			printf("ERROR: Out of memory after %d MiB\n", i);
 			return 1;
 		}
-		else {
-			memcpy(mem[i], (void *) buffer, Mb);
-		  usleep (20000); // Wait 20 microseconds
-	  }
+		else
+		{
+			memcpy(mem[i], (void *)buffer, MiB);
+			usleep(micro_sleep); // Wait 20 microseconds
+		}
 	}
 
 	// Sleep 10 seconds, so that you have time to monitor the full memory allocation
-	sleep (10);
+	printf("Filled %d GiB of memory, sleeping %u seconds.\n", tot_GiB, long_sleep);
+	sleep(long_sleep);
+	printf("Start freeing the memory\n");
 
 	// Free memory, 1 Mb at a time
-  for (i = 0; i < tot_Gb*1024; i++) {
+	for (unsigned int i = 0; i < tot_GiB * 1024; i++)
+	{
 		free(mem[i]);
 	}
-	// Sleep 10 seconds, so that you have time to monitor the freed-up  memory
-	sleep (10);
-  return 0;
-}
+	// Sleep 10 seconds, so that you have time to monitor the freed-up	memory
+	printf("Freed the memory, sleeping %u seconds\n", long_sleep);
+	sleep(long_sleep);
 
+	return 0;
+}
